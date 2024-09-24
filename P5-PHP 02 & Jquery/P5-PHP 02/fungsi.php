9e2b16095e1be34d1fc3aefb0bb6c2ff4273ef36
Ivansyah <ivansyahsantoso@gmail.com>
2024-09-24 08:12:30 +0700

    add: Menambahkan file fungsi.php

---
+++ P5-PHP 02 & Jquery/P5-PHP 02/fungsi.php
@@ -0,0 +1,11 @@
+<?php
+
+function perkenalan(){
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Ivansyah<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
+}
+
+// memanggil fungsi yang sudah dibuat
+perkenalan();
+?>
