f52fb9b4fb1332b6c35858b410b22667f71e6cff
Ivansyah <ivansyahsantoso@gmail.com>
2024-09-24 09:40:47 +0700

    edit: mengedit kode agar membalik string per kata

--- P5-PHP 02 & Jquery/P5-PHP 02/string3.php
+++ P5-PHP 02 & Jquery/P5-PHP 02/string3.php
@@ -1,6 +1,13 @@
 <?php
 
 $pesan = "Saya arek malang";
-echo sttrev($pesan) = "<br>";
+# Ubah variabel $pesan menjadi array dengan perintah explode
+$pesanPerKata = explode(" ", $pesan);
+# ubah setiap kata dalam array menjadi kebalikannya
+$pesanPerKata = array_map(fn($pesan) => strrev($pesan), $pesanPerKata);
+# gabungkan kembali array menjadi string
+$pesan = implode(" ", $pesanPerKata);
+
+echo $pesan . "<br>";
 
 ?>
