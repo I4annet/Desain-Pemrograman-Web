25ebe8b5e550d0b0d413c9057b6ce7515eb901fe
Ivansyah <ivansyahsantoso@gmail.com>
2024-09-24 08:40:42 +0700

    edit: Fungsi yang mengembalikan nilai

--- P5-PHP 02 & Jquery/P5-PHP 02/fungsi.php
+++ P5-PHP 02 & Jquery/P5-PHP 02/fungsi.php
@@ -1,19 +1,24 @@
 <?php
-
-function perkenalan($nama, $salam="Assalamualaikum"){
-    echo $salam. ", ";
-    echo "Perkenalkan, nama saya ".$nama."<br/>";
-    echo "Senang berkenalan dengan anda<br/>";
+function hitungUmur($thn_lahir, $thn_sekarang){
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
 }
 
-// memanggil fungsi yang sudah dibuat
-perkenalan("Hamdana", "Hallo");
+echo "Umur saya adalah ". hitungUmur(2004, 2024) ."tahun";
+// function perkenalan($nama, $salam="Assalamualaikum"){
+//     echo $salam. ", ";
+//     echo "Perkenalkan, nama saya ".$nama."<br/>";
+//     echo "Senang berkenalan dengan anda<br/>";
+// }
 
-echo "<hr>";
+// // memanggil fungsi yang sudah dibuat
+// perkenalan("Hamdana", "Hallo");
 
-$saya = "Ivan";
-$ucapanSalam = "Selamat pagi";
+// echo "<hr>";
 
-// memanggil lagi
-perkenalan($saya);
-?>
+// $saya = "Ivan";
+// $ucapanSalam = "Selamat pagi";
+
+// // memanggil lagi
+// perkenalan($saya);
+
