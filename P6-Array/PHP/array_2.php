b1f7737abb6ad1de3a81fb2e5eec50de45b1869b
Ivansyah <ivansyahsantoso@gmail.com>
2024-10-02 16:48:45 +0700

    edit: Menambahkan style tabel

--- P6-Array/PHP/array_2.php
+++ P6-Array/PHP/array_2.php
@@ -5,6 +5,26 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title></title>
 </head>
+<style>
+  table {
+    width: 50%;
+    margin: 20px auto;
+    border-collapse: collapse;
+  }
+  table, th,td {
+    border: 1px solid black;
+  }
+  th,td {
+    padding: 10px;
+    text-align: left;
+  }
+  th {
+    background-color: #f2f2f2;
+  }
+  td {
+    background-color: #fafafa;
+  }
+</style>
 <body>
   <?php
   $Dosen = [
@@ -12,10 +32,24 @@
       'domisili' => 'Malang',
       'jenis_kelamin' => 'Perempuan'
   ];
-
-  echo "Nama: {$Dosen['nama']} <br>";
-  echo "Domisili: {$Dosen['domisili']} <br>";
-  echo "Jenis Kelamin: {$Dosen['jenis_kelamin']} <br>";
   ?>
+  <table>
+    <tr>
+      <th>Data</th>
+      <th>Informasi</th>
+    </tr>
+    <tr>
+      <td>Nama</td>
+      <td><?php  echo $Dosen['nama']; ?></td>
+    </tr>
+    <tr>
+      <td>Domisili</td>
+      <td><?php  echo $Dosen['domisili']; ?></td>
+    </tr>
+    <tr>
+      <td>Jenis Kelamin</td>
+      <td><?php  echo $Dosen['jenis_kelamin']; ?></td>
+    </tr>
+  </table>
 </body>
 </html>
