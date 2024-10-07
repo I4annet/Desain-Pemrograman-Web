c741f61ff2a0e73d27cd717f6d9edb4cbdf0ae58
Ivansyah <ivansyahsantoso@gmail.com>
2024-10-08 06:53:28 +0700

    add: Menambahkan file empty.php untuk Praktikum 2 Function Empty

---
+++ P7-Form/empty.php
@@ -0,0 +1,13 @@
+<?php
+$myArray = array();
+if (empty($myArray)) {
+    echo "Array tidak terdefinisi atau kosong";
+} else {
+    echo "Array terdefinisi dan tidak kosong";
+}
+if (empty($nonExistenVar)) {
+    echo "Variabel tidak terdefinisi atau kosong";
+} else {
+    echo "variabel terdefinisi dan tidak kosong";
+}
+?>
