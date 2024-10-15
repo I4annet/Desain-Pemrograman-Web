3413ee621f3b8fdbcb95b6729ca3ba0333e226d6
Ivansyah <ivansyahsantoso@gmail.com>
2024-10-15 08:28:03 +0700

    edit: mengedit nama dan usia menjadi terpisah

--- P7-Form/isset.php
+++ P7-Form/isset.php
@@ -1,8 +1,17 @@
 <?php
-$umur;
-if (isset($umur) && $umur >= 18) {
-    echo "Anda sudah dewasa.";
+// $umur;
+// if (isset($umur) && $umur >= 18) {
+//     echo "Anda sudah dewasa.";
+// } else {
+//     echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan";
+// }
+
+$data = array("nama" => "Jane", "usia" => 25);
+if (isset($data["nama"])) {
+    echo "Nama: " . $data["nama"] . "<br>";
+    echo "Usia: " . $data["usia"] . "<br>";
 } else {
-    echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan";
+    echo "Variabel 'nama' tidak ditemukan dalam array" ;
 }
+ 
 ?>
